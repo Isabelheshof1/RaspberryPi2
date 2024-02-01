73a2eb3f27dfeec2fdb861cf9dbce4ef853abfa2
Isabelheshof1 <154346538+Isabelheshof1@users.noreply.github.com>
2024-02-01 12:01:57 +0100

    Update Opdracht8b.ino

--- Opdracht8b.ino
+++ Opdracht8b.ino
@@ -4,7 +4,7 @@ int ledPin10 = 10;
 
 void setup(){
   pinMode(ledPin9, OUTPUT);
-  PinMode(ledPin10, OUTPUT);
+  pinMode(ledPin10, OUTPUT);
 }
 
 //Loop
@@ -12,11 +12,11 @@ void loop(){
 //Led9 laten knipperen 
 digitalWrite(ledPin9, HIGH);
 digitalWrite(ledPin10, LOW);
-delay(3000)
+delay(3000);
 digitalWrite(ledPin9, LOW);
 
 //Led10 laten knipperen
 digitalWrite(ledPin10, HIGH);
-delay(1000)
+delay(1000);
 digitalWrite(ledPin10, LOW);
 }
