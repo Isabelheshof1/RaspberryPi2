948cee13de03fe5c503f43dfd4c944039832eb87
Isabelheshof1 <154346538+Isabelheshof1@users.noreply.github.com>
2024-02-01 12:08:34 +0100

    Update Opdracht8c.ino

--- Opdracht8c.ino
+++ Opdracht8c.ino
@@ -1,7 +1,9 @@
+//Aansluiten vd pinnen 
 const int ledPin9 = 9;
 const int ledPin10 = 10;
 const int buttonPin2 = 2;
 
+//Variablen voor de status van de ledjes en de button
 bool status1 = false;
 bool status2 = false;
 bool statusButton = false;
@@ -19,17 +21,24 @@ pinMode(buttonPin2, INPUT_PULLUP);
 }
 
 void loop(){
+  //Status vd button lezen
 bool buttonState = digitalRead(buttonPin2);
 
+  //Als de knop is ingedrukt voor het volgende uit
 if (buttonState == LOW){
   unsigned long huidigeMillis = millis();
+  //als de huidige tijd min de laatste tijd gelijk is aan 500 
   if (huidigeMillis - laatsteMillis1 >= interval1){
     laatsteMillis1 = huidigeMillis;
+    //de status vd de led wordt gewisseld
     status1 = !status1;
+    //zet de led aan
     digitalWrite(ledPin9, status1);
     }
   } else {
+  //als de knop niet is ingedrukt
     unsigned long huidigeMillis = millis();
+  //Werkt hetzelfde maar dan voor de andere led
   if (huidigeMillis - laatsteMillis2 >= interval2){
     laatsteMillis2 = huidigeMillis;
     status2 = !status2;
