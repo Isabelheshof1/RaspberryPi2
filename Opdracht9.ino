2aa3ff8baeedc0176e54edfb54edbe3ca1ff5748
Isabelheshof1 <154346538+Isabelheshof1@users.noreply.github.com>
2024-02-05 12:26:55 +0100

    Update Opdracht9.ino

--- Opdracht9.ino
+++ Opdracht9.ino
@@ -4,51 +4,60 @@
 //Defineren van de pinnen op de arduino en de hex codes
 const int PinIr = 8;
 const int RasPin[] = {2,3,4,7};
-const unsigned long hex[] = {0xF30CFF00, 0xE718FF00, 0xA15EFF00, 0xF708FF00};
-const int PinNum = sizeof(RasPin) / sizeof(RasPin[0]);
+
+unsigned long hex[] = {0xF30CFF00, 0xE718FF00, 0xA15EFF00, 0xF708FF00};
+
+unsigned long newhex = 0;
+
+//Variable laatstemillis
+unsigned long laatsteMillis[] = {0,0};
+
+int count = 0;
+int laatsteLed;
 
 // Initialseren van de ir-ontvangen
 IRrecv irrecv(PinIr);
 decode_results results;
 
-//Variable laatstemillis
-unsigned long laatsteMillis = 0;
-
 //Inschakelen vd ir
 void setup(){
   irrecv.enableIRIn();
-  for (int i = 0; i < PinNum; i++){
+  for (int i = 0; i < sizeof(RasPin)/ sizeof(RasPin[0]);
+i++){
     pinMode(RasPin[i], OUTPUT);
   }
 }
 
-//loop
-//Als er een infraroodsignaal wordt ontvangen worden de hexdecimale opgevraagd
-//Als deze overeenkomen met de vooraf gedfineerde waarden wordt het tijdstip vd knop bijgehouden
-//De leds worden uitgeschakeld door de functie
-//de led die overeenkomt met de ontvangen worden wordt ingeschakeld
 void loop(){
-  if (irrecv.decode(&results)){
-    unsigned long newhex = results.value;
-    for (int i = 0; i < PinNum; i++){
-      if (newhex == hex[i]) {
-        laatsteMillis = millis();
-        uitzettenPinnen();
+  juisteButton();
+  if (count == 1 && millis() - laatsteMillis[0] >= 2000){
+    count = 0;
+  } 
+}
+
+void uitschakelenLed(){
+  for (int i = 0; i < sizeof(RasPin) / sizeof(RasPin[0]);
+    i++){
+      digitalWrite(RasPin[i], LOW);
+    }
+}
+
+void juisteButton(){
+  if (irrecv.decode()){
+    newhex = irrecv.decodedIRData.decodedRawData;
+    Serial.println(newhex);
+
+    for (int i = 0; i < sizeof( hex) / sizeof(hex[0]); i++){
+      if (newhex == hex[i]){
+        laatsteMillis[0] = millis();
+        uitschakelenLed();
         digitalWrite(RasPin[i], HIGH);
-        break;
+        laatsteLed = i;
       }
     }
     irrecv.resume();
   }
-
-  if (millis() - laatsteMillis >= 2000 && laatsteMillis != 0){
-    uitzettenPinnen();
-    laatsteMillis=0;
-  }
 }
 
-void uitzettenPinnen(){
-  for (int i = 0; i < PinNum; i ++){
-    digitalWrite(RasPin[i], LOW);
-}
-}
+
+ 
